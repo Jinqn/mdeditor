85dbdb60169a64544dc6e0a29247a57c23665467
xujinquan <459452372@qq.com>
2014-05-21 12:03:13 +0800

    add demo

--- src/demo.js
+++ src/demo.js
@@ -1,12 +1,19 @@
 var ue = UE.getEditor('editor');
 
 ue.ready(function(){
+
+    ue.addListener('afterSetContent', function(){
+        var md = ue.getPlainTxt();
+        var blocks = split_blocks(md);
+        debugger;
+    });
+
     ue.setContent('<p># 标题1【a】</p>' +
-        '<p>文本 文本 文本 文本 文本 文本</p>' +
-        '<p>文本 文本 文本 文本 文本 文本</p>' +
+        '<p>文本 **文本** 文本 __文本__ 文本 文本</p>' +
+        '<p>文本 **文本** 文本 文本 文本 文本</p>' +
         '<p><br/></p>' +
         '<p>## 标题2【b】</p>' +
-        '<p>文本 文本 文本 文本 文本 文本</p>' +
+        '<p>文本 **文本** 文本 **文本** 文本 文本</p>' +
         '<p>文本 文本 文本 文本 文本 文本</p>' +
         '<p><br/></p>' +
         '<p>## 标题2【c】</p>' +
@@ -21,9 +28,17 @@ ue.ready(function(){
         '<p>## 标题2【e】</p>' +
         '<p>文本 文本 文本 文本 文本 文本</p>' +
         '<p>文本 文本 文本 文本 文本 文本</p>' +
-        '<p>&gt; 文本 文本 文本 文本 文本 文本</p>' +
+        '<p>&gt; 【f】文本 文本 文本 文本 文本 文本</p>' +
         '<p>&gt; 文本 文本 文本 文本 文本 文本</p>' +
         '<p><br/></p>' +
         '<p><br/></p>' +
         '<p><br/></p>');
+
+
+
 });
+
+function split_blocks(source){
+    var m = new markdown.Markdown();
+    return m.split_blocks(source);
+}
