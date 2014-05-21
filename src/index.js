1e3157fd1021b071c30c8bcd76f019e2d06782ff
xujinquan <459452372@qq.com>
2014-05-21 13:59:16 +0800

    add demo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-var ue = UE.getEditor('editor');
+// markdown解析器
+UE.md2html = function(md){
+    return window.markdown.toHTML(md);
+};
+UE.html2md = function(html){
+    return window.toMarkdown(html);
+};
 
-ue.ready(function(){
+// 实例化ueditor
+var ue1 = UE.getEditor('editor1');
+var ue2 = UE.getEditor('editor2');
 
-    ue.addListener('afterSetContent', function(){
-        var md = ue.getPlainTxt();
-        var blocks = split_blocks(md);
-        console.log(blocks);
+ue1.ready(function(){
+
+    var preContent = '';
+
+    ue1.addListener('contentchange', function(){
+        var md = ue1.getPlainTxt();
+        var html = UE.md2html(md);
+        ue2.setContent(html);
     });
 
-    ue.setContent('<p># 标题1【a】</p>' +
+    ue1.setContent('<p># 标题1【a】</p>' +
         '<p>文本 **文本** 文本 __文本__ 文本 文本</p>' +
         '<p>文本 **文本** 文本 文本 文本 文本</p>' +
         '<p><br/></p>' +
@@ -30,12 +42,13 @@ ue.ready(function(){
         '<p>文本 文本 文本 文本 文本 文本</p>' +
         '<p>&gt; 【f】文本 文本 文本 文本 文本 文本</p>' +
         '<p>&gt; 文本 文本 文本 文本 文本 文本</p>' +
+        '<p>| 标题 | 标题 | 标题 |</p>' +
+        '<p>| -- | -- | -- |</p>' +
+        '<p>| 内容 | 内容 | 内容 |</p>' +
+        '<p>| 内容 | 内容 | 内容 |</p>' +
         '<p><br/></p>' +
         '<p><br/></p>' +
         '<p><br/></p>');
-
-
-
 });
 
 function split_blocks(source){
